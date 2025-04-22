af87a861cf2eeb8b598beed2446ea8117b99bc32
teren <teren@KOMP>
2025-04-22 17:18:11 +0200

    Dodaj pliki projektów.

---
+++ muzyka/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace muzyka
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ muzyka/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace muzyka
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
